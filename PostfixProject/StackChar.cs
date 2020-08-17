46c39b5fb67c7843b3ad7083c50d7c0caa736488
MazaharAhamed <mazaharahamed77@gmail.com>
2020-08-17 19:34:16 +0530

    Add project files.

---
+++ PostfixProject/StackChar.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostfixProject
+{
+    class StackChar
+    {
+        private char[] stackArray;
+        private int top;
+
+        public StackChar()
+        {
+            stackArray = new char[10];
+            top = -1;
+        }
+
+        public StackChar(int maxSize)
+        {
+            stackArray = new char[maxSize];
+            top = -1;
+        }
+
+        public bool isEmpty()
+        {
+            return (top == -1);
+        }
+
+        public bool isFull()
+        {
+            return (top == stackArray.Length - 1);
+        }
+
+        public void Push(char x)
+        {
+            if (isFull())
+            {
+                Console.WriteLine("Stack Overflow");
+                return;
+            }
+            top = top + 1;
+            stackArray[top] = x;
+        }
+
+        public char Pop()
+        {
+            char x;
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack Underflow");
+            }
+            x = stackArray[top];
+            top = top - 1;
+            return x;
+        }
+
+        public char Peek()
+        {
+            if (isEmpty())
+                throw new System.InvalidOperationException("Stack Underflow");
+            return stackArray[top];
+        }
+    }
+}
---
+++ PostfixProject/StackInt.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostfixProject
+{
+    class StackInt
+    {
+        private int[] stackArray;
+        private int top;
+
+        public StackInt()
+        {
+            stackArray = new char[10];
+            top = -1;
+        }
+
+        public StackInt(int maxSize)
+        {
+            stackArray = new char[maxSize];
+            top = -1;
+        }
+
+        public bool isEmpty()
+        {
+            return (top == -1);
+        }
+
+        public bool isFull()
+        {
+            return (top == stackArray.Length - 1);
+        }
+
+        public void Push(int x)
+        {
+            if (isFull())
+            {
+                Console.WriteLine("Stack Overflow");
+                return;
+            }
+            top = top + 1;
+            stackArray[top] = x;
+        }
+
+        public int Pop()
+        {
+            int x;
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack Underflow");
+            }
+            x = stackArray[top];
+            top = top - 1;
+            return x;
+        }
+
+        public int Peek()
+        {
+            if (isEmpty())
+                throw new System.InvalidOperationException("Stack Underflow");
+            return stackArray[top];
+        }
+    }
+}
